ccd48606d6b3986d2e629e2844af865805fbaa04
gabrielvf64 <gabrielvf64@gmail.com>
2020-01-25 11:42:05 -0300

    Add body-parser to read the body data; Add post route

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -2,4 +2,9 @@ module.exports = app => {
   app.get('/appointments', (req, res) =>
     res.send('Appointments route ok. update test')
   );
+
+  app.post('/appointments', (req, res) => {
+    console.log(req.body);
+    res.send('post test');
+  });
 };
