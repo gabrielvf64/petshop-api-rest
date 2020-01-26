693a4d2e8e503dbfc7da9e68ea720461dd7888ff
gabrielvf64 <gabrielvf64@gmail.com>
2020-01-26 17:26:42 -0300

    Created class Tables; Created table with its columns

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const customExpress = require('./config/customExpress');
-
 const connection = require('./infra/connection');
+const Tabelas = require('./infra/tables');
 
 connection.connect(error => {
   if (error) {
     console.log(error);
   } else {
     console.log('Connected to the database');
+
+    Tabelas.init(connection);
 
     const app = customExpress();
 
