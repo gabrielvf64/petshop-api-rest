fc1808593948d8e87dc458e424b2d3e7a2943b7f
gabrielvf64 <gabrielvf64@gmail.com>
2020-01-29 10:48:36 -0300

    Created errors messages and two types of validations

--- models/appointments.js
+++ models/appointments.js
@@ -10,21 +10,46 @@ class Appointments {
       'YYYY-MM-DD HH:MM:SS'
     );
 
-    const atendimentoDatado = {
-      ...appointment,
-      dataCriacao,
-      data
-    };
+    const validDate = moment(data).isSameOrAfter(dataCriacao);
 
-    const sql = 'INSERT INTO appointments SET ?';
+    const validClientName = appointment.client.length >= 5;
 
-    connection.query(sql, atendimentoDatado, (error, results) => {
-      if (error) {
-        res.status(400).json(error);
-      } else {
-        res.status(201).json(results);
+    const validations = [
+      {
+        name: 'data',
+        isValid: validDate,
+        message: 'The date must be equals or greater than the current date'
+      },
+      {
+        name: 'client',
+        isValid: validClientName,
+        message: 'The client must have the minimum of 5 characteres'
       }
-    });
+    ];
+
+    const errors = validations.filter(field => !field.isValid);
+
+    const hasErrors = errors.length;
+
+    if (hasErrors) {
+      res.status(400).json(errors);
+    } else {
+      const atendimentoDatado = {
+        ...appointment,
+        dataCriacao,
+        data
+      };
+
+      const sql = 'INSERT INTO appointments SET ?';
+
+      connection.query(sql, atendimentoDatado, (error, results) => {
+        if (error) {
+          res.status(400).json(error);
+        } else {
+          res.status(201).json(results);
+        }
+      });
+    }
   }
 }
 
