40ecf984ebbb1e6e1c581cd83d789ad948f38727
gabrielvf64 <gabrielvf64@gmail.com>
2020-01-29 10:03:20 -0300

    Inserting and formating data columns

--- models/appointments.js
+++ models/appointments.js
@@ -1,10 +1,24 @@
+const moment = require('moment');
+
 const connection = require('../infra/connection');
 
 class Appointments {
   add(appointment) {
+    const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
+
+    const data = moment(appointment.data, 'DD/MM/YYYY').format(
+      'YYYY-MM-DD HH:MM:SS'
+    );
+
+    const atendimentoDatado = {
+      ...appointment,
+      dataCriacao,
+      data
+    };
+
     const sql = 'INSERT INTO appointments SET ?';
 
-    connection.query(sql, appointment, (error, results) => {
+    connection.query(sql, atendimentoDatado, (error, results) => {
       if (error) {
         console.log(error);
       } else {
